1ba6a3cb053917c65f456ff07cf545fbd38d54a3
Francis Albuna <falbuna1@gmail.com>
2020-09-13 13:12:01 -0700

    Added routes folder to route the html files.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function(){
     console.log("App is listening on PORT: " + PORT);
